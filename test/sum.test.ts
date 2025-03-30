417ed57f305df744781a64dea0eccb4943a75664
gmvrpw <dev@gmvr.pw>
2025-03-30 22:06:31 +0300

    make breaking change

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -3,8 +3,8 @@ import { expect, test } from 'vitest';
 import { sum } from '../src';
 
 test.each([
-  { a: 2, b: 2, result: 4 },
-  { a: -1, b: 1, result: 0 },
-])('should correctly summarize two numbers', ({ a, b, result }) => {
-  expect(sum(a, b)).toBe(result);
+  { a: 2, b: 2, c: 2, result: 6 },
+  { a: -1, b: 1, c: 1, result: 1 },
+])('should correctly summarize two numbers', ({ a, b, c, result }) => {
+  expect(sum(a, b, c)).toBe(result);
 });
